b7cf5edd81ad519fab4ee26ee5143c3c4242e380
Daler Asrorov <asrorids@gmail.com>
2015-10-11 23:23:44 -0700

    Implemented the events search and mapping

--- A4 - Position Locator/main.js
+++ A4 - Position Locator/main.js
@@ -1,74 +1,74 @@
-var getPosition = navigator.geolocation.getCurrentPosition(function(position) {
+// var getPosition = navigator.geolocation.getCurrentPosition(function(position) {
 
-	//getting the current location of the user
-	var center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+// 	//getting the current location of the user
+// 	var center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-	//creating the map and setting it up to the current location (before the reverse geocoding)
-	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-		center: center,
-		zoom: 5
-	});
+// 	//creating the map and setting it up to the current location (before the reverse geocoding)
+// 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+// 		center: center,
+// 		zoom: 5
+// 	});
 
-	//creating the new information window and setting it up
-	var infowindow = new google.maps.InfoWindow({
-		content: '',
-		position: center
-	});
+// 	//creating the new information window and setting it up
+// 	var infowindow = new google.maps.InfoWindow({
+// 		content: '',
+// 		position: center
+// 	});
 
-	//create a new instance of the Goecoder class 
-	var geocoder = new google.maps.Geocoder(); 
+// 	//create a new instance of the Goecoder class 
+// 	var geocoder = new google.maps.Geocoder(); 
 
-	//calling the method that reverse geocodes the current location
-	reverseGeocode(geocoder, map, infowindow, position); 
+// 	//calling the method that reverse geocodes the current location
+// 	reverseGeocode(geocoder, map, infowindow, position); 
 
-}, function(position) {
-	window.alert("The map couldn't be loaded");
-});
+// }, function(position) {
+// 	window.alert("The map couldn't be loaded");
+// });
 
-function reverseGeocode(geocoder, map, infowindow, position) {
+// function reverseGeocode(geocoder, map, infowindow, position) {
 
-	//giving the received values to the object 
-	var latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+// 	//giving the received values to the object 
+// 	var latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
 
-	//reverse geocoding the current location
-	geocoder.geocode({'location': latlng}, function(results, status) {
+// 	//reverse geocoding the current location
+// 	geocoder.geocode({'location': latlng}, function(results, status) {
 
-	//if the location is found, show the results	
-    if (status === google.maps.GeocoderStatus.OK) {
-       if(results[0]) {
+// 	//if the location is found, show the results	
+//     if (status === google.maps.GeocoderStatus.OK) {
+//        if(results[0]) {
 
-       	//geting the result's latitude and longtitude
-       	//and giving these values to the new object
-       	var newLat = results[0].geometry.location.lat();
-       	var newLng = results[0].geometry.location.lng();
-    		var newLatLng = {lat: newLat, lng: newLng};	
+//        	//geting the result's latitude and longtitude
+//        	//and giving these values to the new object
+//        	var newLat = results[0].geometry.location.lat();
+//        	var newLng = results[0].geometry.location.lng();
+//     		var newLatLng = {lat: newLat, lng: lnewLng};	
 
-    		//method to zoom in the map
-       		map.setZoom(16);  
+//     		//method to zoom in the map
+//        		map.setZoom(16);  
        		
-       		//location of the icon
-       		var iconBase = 'android.png';
+//        		//location of the icon
+//        		var iconBase = 'android.png';
 
-       		//created the new marker with animation and custom icon
-       		var marker = new google.maps.Marker({
-       			position: newLatLng,
-       			map: map,
-       			icon: iconBase, //icon is set
-       			animation: google.maps.Animation.DROP
-       		});
+//        		//created the new marker with animation and custom icon
+//        		var marker = new google.maps.Marker({
+//        			position: newLatLng,
+//        			map: map,
+//        			icon: iconBase, //icon is set
+//        			animation: google.maps.Animation.DROP
+//        		});
        		
-       		//if the user clicks on the marker, show him the address
-       		google.maps.event.addListener(marker, 'click', function(event) {
-       			infowindow.setContent(results[0].formatted_address);
-       			infowindow.open(map, marker);
-			});
-       }
-       else {
-       		window.alert('No results found');
-       }
-    } 
-    else {
-    	window.alert('Geocoder failed due to ' + status);
-    }
-  });
-};
+//        		//if the user clicks on the marker, show him the address
+//        		google.maps.event.addListener(marker, 'click', function(event) {
+//        			infowindow.setContent(results[0].formatted_address);
+//        			infowindow.open(map, marker);
+// 			});
+//        }
+//        else {
+//        		window.alert('No results found');
+//        }
+//     } 
+//     else {
+//     	window.alert('Geocoder failed due to ' + status);
+//     }
+//   });
+// };
---
+++ Midterm Project/js/main.js
@@ -0,0 +1,76 @@
+var app = angular.module('myApp', ['ngSanitize']);
+
+var name = "Daler Asrorov";
+var artist = "Limp Bizkit"; 
+var lat = 30.141198;
+var lon = -38.787720;
+
+var center = new google.maps.LatLng(lat, lon); //3​0.141198, ­38.787720
+
+//creating the map and setting it up to the given location: 3​0.141198, ­38.787720
+var map = new google.maps.Map(document.getElementById('map-canvas'), {
+	center: center,
+	zoom: 5
+});
+
+var plotPoints = function(locObj) {
+	var myLatlng = new google.maps.LatLng(parseFloat(locObj.lat),parseFloat(locObj.lon));
+	//created the new marker with animation and custom icon
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		animation: google.maps.Animation.DROP
+	});
+};
+
+app.controller('NavbarCtrl', function($scope) {
+  $scope.hMain = "Midterm Project";
+  $scope.hOne = "App";
+  $scope.hTwo = "Github";
+  $scope.hThree = "Doc";
+  $scope.hLeft = "by " + name;
+});
+
+app.controller('EventsSearchCtrl', function($scope, Events, Location){
+	  Events.search(artist).then(function(events) {
+      	$scope.events = events;
+      //	console.log(events.event);
+      	$scope.listOfEvents = events.event; //an object that contains arrays
+
+      	$scope.listOfEvents.forEach(function(event) {
+      		$scope.locationObj = Location.getLocation(event); //getting the location object
+      		plotPoints($scope.locationObj);
+      		map.setZoom(3);
+  		});
+    });
+});
+
+//getEvents();
+app.factory('Events', function($http) {
+	return {
+	    search: function(artist) {
+	      //http://api.eventful.com/json/events/search?c=music&app_key=NpmnLBfV4QKQtQ2N&page_number=1&date=Future&keywords=limp%20bizkit&callback=processJSONP	
+	      var url = 'http://api.eventful.com/json/events/search?c=music&app_key=NpmnLBfV4QKQtQ2N&page_number=1&date=Future&keywords='+ artist 
+	      + '&callback=JSON_CALLBACK';
+
+	      return $http.jsonp(url).then(function(response) {
+	         	return response.data.events;
+	      });
+	    }
+  };
+});
+
+app.factory('Location', function() {
+	return {
+		getLocation: function(event) {
+			console.log(event);
+			var loc = {
+				lat: event.latitude,
+				lon: event.longitude
+			};
+
+			return loc;
+		}
+	};
+});
+
