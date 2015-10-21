775cdd13082bc088b8b97b9b6e608b46ba9d8b57
David Tang <dtang859@gmail.com>
2015-10-21 00:14:50 -0700

    starter files

--- controllers/product.js
+++ controllers/product.js
@@ -3,9 +3,4 @@ angular
   .controller('ProductController', function(product) {
     var vm = this;
     vm.product = product;
-
-    vm.save = function(product, rating) {
-      console.log('saving with ajax...', product, rating);
-      product.rating = rating;
-    };
   });
